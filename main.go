8460c8662c53462ec31956cdce4a4eb02f8ab353
Nikita Gorbunov <k0tletka@thunix.net>
2022-10-21 01:22:06 +0600

    Added index.html and .gitignore

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"MinecraftServerLogger/config"
+	"MinecraftServerLogger/httpserv"
 	"MinecraftServerLogger/logger"
 	"log"
 )
@@ -9,11 +10,16 @@ import (
 func main() {
 	cfg, err := config.GetConfiguration()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("GetConfiguration: ", err)
 	}
 
 	tailLogger := logger.NewLogger(cfg)
 	if err := tailLogger.Start(); err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Logger: ", err)
+	}
+
+	server := httpserv.NewLoggerHTTPServer(cfg, tailLogger)
+	if err := server.StartServer(); err != nil {
+		log.Fatalln("HTTPServer: ", err)
 	}
 }
